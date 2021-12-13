f6a0e209d27878544f5f44eff8795a7cca76d94c
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-13 11:13:34 +1100

    Update Blog.jsx

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Article from '../../components/article/Article'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 
 const Blog = () => {
     return (
